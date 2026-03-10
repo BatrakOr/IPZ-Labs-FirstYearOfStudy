37aaa0f11207f591742afc0510e3a81637fdb9ed
Орина Батрак <arbatrakk@gmail.com>
2026-03-10 20:13:40 +0200

    Updated main.cpp, added Faculty class

--- Lab2, types of classes/main.cpp
+++ Lab2, types of classes/main.cpp
@@ -2,23 +2,28 @@
 #include<string>
 #include "Student.h"
 #include "Teacher.h"
+#include "Faculty.h"
 
 using namespace std;
 
 int main() {
     Teacher t1;
 
-    Teacher t2("Diakonenko Bohdan Viktorovich", "OOP", 99, true);
+    Teacher t2("Diakonenko Bohdan Viktorovich", "IFTKN", 1, true);
 
-    Teacher t3("Val Oleksandr Olekasandrovich", "C++ Lectures", 97, true);
+    Teacher t3("Val Oleksandr Olekasandrovich", "IFTKN", 99, true);
 
     Student s1;
 
-    Student s3("Oleh", "Mathematics", 3, true);
+    Student s3("Mariana", "IPZ", 3, true);
 
     Student s2("Andriy", "Mathematics", 1, true);
 
-    cout << endl;
+    Faculty f1();
+
+    Faculty f2(83, 143, "Kohut Oleh");
+
+    Faculty f3(67, 999, "Bruce Wayne");
 
     s1.printInfo();
     cout << endl;
@@ -35,8 +40,13 @@ int main() {
     cout << endl;
 
     t3.printInfo();
+    cout << endl;
 
+    f3.printInfo();
+    cout << endl;
 
+    f2.printInfo();
+    cout << endl;
 
-    return 0;
+ return 0;
 }
