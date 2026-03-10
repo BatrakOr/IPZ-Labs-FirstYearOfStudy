02259964181e31fb04a7149d5fec7d152f129857
Орина Батрак <arbatrakk@gmail.com>
2026-03-10 18:12:41 +0200

    Just add main file

---
+++ Lab2, types of classes/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<string>
+#include "Student.h"
+
+using namespace std;
+
+int main() {
+
+    Student s1;
+
+    Student s3("Oleh", "Mathematics", 3, true);
+
+    Student s2("Andriy", "Mathematics", 1, true);
+
+    cout << endl;
+
+    s1.printInfo();
+    cout << endl;
+
+    s2.printInfo();
+    cout << endl;
+
+    s3.printInfo();
+
+    return 0;
+}
