bd1863d9e9950c50babef0f2bf210dac9a1344f4
Орина Батрак <arbatrakk@gmail.com>
2026-03-10 18:19:58 +0200

    Just add Student.h

---
+++ Lab2, types of classes/Student.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+
+private:
+    std::string NameOfStudent;
+    std::string faculty;
+    int yearOfStudy;
+
+public:
+
+    Student();
+    Student(std::string name, std::string fac, int year);
+    Student(std::string name, std::string fac, int year, bool message);
+
+    ~Student();
+
+    void printInfo();
+};
+
+#endif
