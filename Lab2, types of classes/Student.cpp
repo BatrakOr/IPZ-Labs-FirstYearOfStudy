505c40b87a1a808ef7dbc4b1144896b7f37e933b
Орина Батрак <arbatrakk@gmail.com>
2026-03-21 21:55:10 +0200

    Just add copy constructor to Student class(in cpp file)

--- Lab2, types of classes/Student.cpp
+++ Lab2, types of classes/Student.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 Student::Student()
     : NameOfStudent("Unknown"), faculty("Unknown"), yearOfStudy(1) {
     cout << "Default constructor called" << endl;
@@ -12,6 +13,15 @@ Student::Student()
 Student::Student(string name, string fac, int year)
     : NameOfStudent(name), faculty(fac), yearOfStudy(year) {
     cout << "Constructor with parameters called" << endl;
+}
+
+// Конструктор копирования
+Student::Student(const Student& Ref) :
+    NameOfStudent(Ref.NameOfStudent), faculty(Ref.faculty), yearOfStudy(Ref.yearOfStudy)
+    // Примечание: Мы имеем прямой доступ к членам объекта drob, поскольку мы сейчас находимся внутри класса Drob
+{
+    // Нет необходимости выполнять проверку denominator здесь, так как эта проверка уже осуществляется в конструкторе класса Drob
+    std::cout << "Copy constructor worked here!\n"; // просто, чтобы показать, что это работает
 }
 
 Student::Student(string name, string fac, int year, bool message)
